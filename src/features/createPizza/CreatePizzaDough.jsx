9c2ead3d552a99adac07fef74f2788cb5e3be25c
alidadashzadeh <a.dadashzadeh89@gmail.com>
2024-01-21 19:01:45 -0500

    transfer

--- src/features/createPizza/CreatePizzaDough.jsx
+++ src/features/createPizza/CreatePizzaDough.jsx
@@ -1,11 +1,40 @@
+import styled from "styled-components";
+
+const DoughTypeContainer = styled.div`
+  width: 200px;
+  height: 150px;
+  position: relative;
+`;
+
+const DoughDetails = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
 function CreatePizzaDough() {
-	return (
-		<div>
-			<span>Regular</span>
-			<span>Whole Wheat</span>
-			<span>Gluten Free Multigrain</span>
-		</div>
-	);
+  return (
+    <div>
+      <DoughTypeContainer>
+        <img src="regular-dough.jpg" />
+
+        <DoughDetails>
+          <span>Regular</span>
+        </DoughDetails>
+      </DoughTypeContainer>
+      <DoughTypeContainer>
+        <img src="whole-wheat-dough.jpg" />
+        <DoughDetails>
+          <span>Whole Wheat</span>
+        </DoughDetails>
+      </DoughTypeContainer>
+      <DoughTypeContainer>
+        <img src="multigrain-pizza-dough.jpg" />
+        <DoughDetails>
+          <span>Gluten Free Multigrain</span>
+        </DoughDetails>
+      </DoughTypeContainer>
+    </div>
+  );
 }
 
 export default CreatePizzaDough;
